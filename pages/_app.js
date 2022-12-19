a644d832424ab09a55329e9040828169b0a39a9e
qedgary <32651742+qedgary@users.noreply.github.com>
2022-12-18 21:52:26 -0500

    global CSS for site

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// This sets the global CSS styles for all pages on the site
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
