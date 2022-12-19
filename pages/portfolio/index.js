9630c8230a3905d23dbd8ca91e69c7c6e8d10eec
qedgary <32651742+qedgary@users.noreply.github.com>
2022-12-18 21:53:08 -0500

    draft of portfolio

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+export default function Portfolio() {
+    return <>
+        <h2>Portfolio</h2>
+        <h3>Print design</h3>
+        <h3>Social media</h3>
+        <h3>Video</h3>
+        <h3>Web design</h3>
+    </>;
+}
